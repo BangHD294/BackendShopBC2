c82717ce9228a6bda90193c5fc3c899bae51227c
Hoang Dinh Bang <hoangbang2941994@gmail.com>
2021-11-11 22:06:52 +0700

    add login logout Laravel Breeze-laravel 8

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <button class="flex items-cDashboardenter text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
@@ -34,7 +34,7 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <!-- Authentication -->
+                        <!-- Authentication checklogout-->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
@@ -76,7 +76,7 @@
             </div>
 
             <div class="mt-3 space-y-1">
-                <!-- Authentication -->
+                <!-- Authentication hahahhasdas  -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+//Route::get('/home', function () {
+//    return view('home');
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
 require __DIR__.'/auth.php';
